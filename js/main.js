bc80fae1e26fba2cf7b32607109ef32428737be9
Leydi Velazco <leydi.velazco@outlook.es>
2022-04-16 22:56:49 -0500

    upload project

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//--------- Navbar
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 0) {
+        $('.navbar').css('background', '#382d34')
+    } else {
+        $('.navbar').css('background', 'transparent')
+    }
+});
+
+//--------- Comments
+$('.comment__sider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    items: 1,
+    smartSpeed: 700,
+})
+
+//--------- Blog
+$('.blog__slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    smartSpeed: 700,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
